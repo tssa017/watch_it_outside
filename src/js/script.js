fffd2a7825236c19c2294f570afc33b72f7cacd4
malonetheressa <theressadmalone@gmail.com>
2023-09-23 12:31:01 +0200

    :sparkles: test modal

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// Function to open the modal
+function openModal() {
+    document.getElementById('myModal').classList.remove('hidden');
+}
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById('myModal').classList.add('hidden');
+}
